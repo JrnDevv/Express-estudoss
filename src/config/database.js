d4a025ba87ead51132ce3ee9e28e3c650adc6f88
Gabriel Souza <teenjrn@gmail.com>
2026-01-05 00:35:57 -0300

    algumas coisas ai

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import sqlite from "sqlite";
+
+const db = new sqlite3.Database("library_db.sqlite", (err) =>{
+    if (err) {
+        console.log("Error ao Conectar ao banco de dados:", err.mesage)
+    } else {
+        console.log("Conectado ao banco de dados SQlite.")
+    }
+})
+
+export default db;
